557d9f37dbec85a92adaa222025c591326f4231b
krishna vishwakama <146770906+krishnaprince7@users.noreply.github.com>
2024-05-10 17:51:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(cors(
     {
-        origin:["https://krishna-food-website.vercel.app/"],
+        origin:["https://krishna-food-website.vercel.app"],
         methods:["POST","GET"],
         credentials:true
     }
